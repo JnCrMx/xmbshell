c9a955d9d1a0bab86008af3511ae3758a5a40654
JCM <jcm@jcm.re>
2024-04-23 03:27:18 +0200

    CI: download icons not included in the repo

---
+++ .woodpecker/download_unlicensed.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p icons sounds
+
+curl --output icons/icon_category_users.png "https://www.psdevwiki.com/ps3/images/archive/4/47/20140220101344%21Icon_category_users.png"
+curl --output icons/icon_category_settings.png "https://www.psdevwiki.com/ps3/images/archive/b/ba/20140220101323%21Icon_category_settings.png"
+curl --output icons/icon_category_photo.png "https://www.psdevwiki.com/ps3/images/archive/6/61/20140220101240%21Icon_category_photo.png"
+curl --output icons/icon_category_music.png "https://www.psdevwiki.com/ps3/images/archive/2/2b/20140220101209%21Icon_category_music.png"
+curl --output icons/icon_category_video.png "https://www.psdevwiki.com/ps3/images/archive/c/c3/20140220101141%21Icon_category_video.png"
+curl --output icons/icon_category_tv.png "https://www.psdevwiki.com/ps3/images/archive/2/27/20140220101110%21Icon_category_tv.png"
+curl --output icons/icon_category_game.png "https://www.psdevwiki.com/ps3/images/archive/a/af/20140220101052%21Icon_category_game.png"
+curl --output icons/icon_category_network.png "https://www.psdevwiki.com/ps3/images/archive/e/e6/20140220101001%21Icon_category_network.png"
+curl --output icons/icon_category_friends.png "https://www.psdevwiki.com/ps3/images/archive/f/f4/20140220100934%21Icon_category_friends.png"
