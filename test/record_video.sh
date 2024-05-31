f4cab21785de9b564347a00c2743a1c7672bc978
JCM <jcm@jcm.re>
2024-05-31 23:13:18 +0200

    CI: convert mkv to mp4

--- test/record_video.sh
+++ test/record_video.sh
@@ -7,3 +7,5 @@ sleep 15
 ffmpeg -threads 1 -video_size $RESOLUTION -framerate 30 -f x11grab -i :99 -preset ultrafast -qp 0 -t 45 -y build/test-output.mkv &
 xdotool key --delay 500 Up Down Right Return Down Down Up Escape Down Return Down Up Escape Right Right Right Right Right Down Down Down Up Up Up
 wait
+
+ffmpeg -threads 1 -i build/test-output.mkv build/test-output.mp4
