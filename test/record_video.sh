bd433729f3594b673e66970578e488f15e301463
JCM <jcm@jcm.re>
2024-10-11 04:04:53 +0200

    Hide mouse from video

--- test/record_video.sh
+++ test/record_video.sh
@@ -4,7 +4,7 @@ source test/setup_recording.sh
 
 dbus-launch timeout 60 ./build/xmbshell | tee build/test-log.txt &
 sleep 15
-ffmpeg -threads 1 -video_size $RESOLUTION -framerate 30 -f x11grab -i :99 -preset ultrafast -qp 0 -t 45 -y build/test-output.mkv &
+ffmpeg -threads 1 -video_size $RESOLUTION -framerate 30 -f x11grab -draw_mouse 0 -i :99 -preset ultrafast -qp 0 -t 45 -y build/test-output.mkv &
 xdotool key --delay 500 Up Down Right Return Down Down Up Escape Down Return Down Up Escape Right Right Right Right Right Down Down Down Up Up Up
 wait
 
