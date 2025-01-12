5a52868558fdeac82c1fe070db585eb6e71df5b3
JCM <jcm@jcm.re>
2025-01-12 17:29:38 +0100

    CI: use headless rendering in test

---
+++ test/headless.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+format_spec="05d"
+
+export DREAMRENDER_HEADLESS=1
+export DREAMRENDER_HEADLESS_WIDTH=800
+export DREAMRENDER_HEADLESS_HEIGHT=600
+export DREAMRENDER_HEADLESS_OUTPUT_DIR="output/"
+export DREAMRENDER_HEADLESS_OUTPUT_PATTERN="{:$format_spec}.jpg"
+export DREAMRENDER_HEADLESS_OUTPUT_QUALITY=90
+
+export GSETTINGS_SCHEMA_DIR="$PWD/schemas:$GSETTINGS_SCHEMA_DIR"
+export XMB_ASSET_DIR=.
+
+duration=30
+dbus-launch timeout $duration ./build/xmbshell | tee build/test-log.txt
+
+frames=$(ls -1q $DREAMRENDER_HEADLESS_OUTPUT_DIR | wc -l)
+framerate=$(($frames / $duration))
+
+echo "Duration: $duration"
+echo "Frames: $frames"
+echo "Framerate: $framerate"
+
+ffmpeg -framerate $framerate \
+    -i "$DREAMRENDER_HEADLESS_OUTPUT_DIR"/"%$format_spec.jpg" \
+    build/test-output.mkv build/test-output.webm \
+    -loop 65535 build/test-output.webp
+
+rm -r "$DREAMRENDER_HEADLESS_OUTPUT_DIR"
