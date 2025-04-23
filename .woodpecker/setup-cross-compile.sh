42996339e816f72fc19ef864a114d91b218c7966
JCM <jcm@jcm.re>
2025-04-23 03:27:39 +0200

    CI: fix cross compile script

--- .woodpecker/setup-cross-compile.sh
+++ .woodpecker/setup-cross-compile.sh
@@ -8,16 +8,25 @@ fi
 
 dpkg --add-architecture $TARGETARCH
 
-if [ -f /etc/apt/sources.list.d/ubuntu.sources ]; then
-    sed -i "/Types: deb/a Architectures: $BUILDARCH" /etc/apt/sources.list.d/ubuntu.sources
+sed -i "/Types: deb/a Architectures: $BUILDARCH" /etc/apt/sources.list.d/ubuntu.sources
+SUITES=$(cat /etc/apt/sources.list.d/ubuntu.sources | grep -E '^[^#]' | grep -Eo 'Suites: .*' | cut -d' ' -f2- | tr '\n' ' ')
+
+URL=""
+if [ "$TARGETARCH" == "amd64" ] || [ "$TARGETARCH" == "i386" ]; then
+    URL="http://archive.ubuntu.com/ubuntu/"
+else
+    URL="http://ports.ubuntu.com/"
 fi
 
-VERSION_CODENAME=$(cat /etc/os-release | grep VERSION_CODENAME | cut -d'=' -f2)
-if [ "$TARGETARCH" == "amd64" ]; then
-    echo "deb [arch=$TARGETARCH] http://archive.ubuntu.com/ubuntu/ $VERSION_CODENAME main universe multiverse" >> /etc/apt/sources.list.d/forein.list
-else
-    echo "deb [arch=$TARGETARCH] http://ports.ubuntu.com/ $VERSION_CODENAME main universe multiverse" >> /etc/apt/sources.list.d/forein.list
-fi
+cat > /etc/apt/sources.list.d/ubuntu-$TARGETARCH.sources <<EOF
+Types: deb
+Architectures: $TARGETARCH
+URIs: $URL
+Suites: $SUITES
+Components: main universe restricted multiverse
+Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
+EOF
+
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends dpkg-dev
 
