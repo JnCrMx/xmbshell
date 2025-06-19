c032853fc51a4a0ff62567f8191b99dc98c7dc77
jcm <jcm@jcm.re>
2025-06-19 21:08:02 +0000

    test: reduce duration
    hopefully this will fix timeout during encoding

--- test/headless.sh
+++ test/headless.sh
@@ -12,7 +12,7 @@ export GSETTINGS_SCHEMA_DIR="$PWD/schemas:$GSETTINGS_SCHEMA_DIR"
 export XMB_ASSET_DIR=.
 export SPDLOG_LEVEL=debug
 
-duration=30
+duration=15
 dbus-launch timeout $duration ./build/xmbshell < /dev/null | tee build/test-log.txt
 
 frames=$(ls -1q $DREAMRENDER_HEADLESS_OUTPUT_DIR | wc -l)
