bd10159c235a4976ee62994db7958717b1d30ca7
JCM <jcm@jcm.re>
2025-06-20 16:36:27 +0200

    test: add input sequence

--- test/headless.sh
+++ test/headless.sh
@@ -12,8 +12,48 @@ export GSETTINGS_SCHEMA_DIR="$PWD/schemas:$GSETTINGS_SCHEMA_DIR"
 export XMB_ASSET_DIR=.
 export SPDLOG_LEVEL=debug
 
-duration=10
-dbus-launch timeout $duration ./build/xmbshell < /dev/null | tee build/test-log.txt
+key_up() {
+    echo "keyboard down 82 1073741906 0\nkeyboard up 82 1073741906 0"
+}
+key_down() {
+    echo "keyboard down 81 1073741905 0\nkeyboard up 81 1073741905 0"
+}
+key_left() {
+    echo "keyboard down 80 1073741904 0\nkeyboard up 80 1073741904 0"
+}
+key_right() {
+    echo "keyboard down 79 1073741903 0\nkeyboard up 79 1073741903 0"
+}
+key_return() {
+    echo "keyboard down 40 13 0\nkeyboard up 40 13 0"
+}
+key_escape() {
+    echo "keyboard down 41 27 0\nkeyboard up 41 27 0"
+}
+
+duration=15
+( \
+    sleep 1.0; key_down; \
+    sleep 0.5; key_up; \
+    sleep 0.5; key_right; \
+    sleep 0.5; key_return; \
+    sleep 0.5; key_down; \
+    sleep 0.5; key_return; \
+    sleep 0.5; key_down; \
+    sleep 0.5; key_up; \
+    sleep 0.5; key_escape; \
+    sleep 0.5; key_escape; \
+    sleep 0.5; key_down; \
+    sleep 0.5; key_return; \
+    sleep 0.5; key_return; \
+    sleep 0.5; key_escape; \
+    sleep 0.5; key_escape; \
+    sleep 0.5; key_left; \
+    sleep 0.5; key_down; \
+    sleep 0.5; key_return; \
+    sleep 0.5; key_right; \
+    sleep 0.5; key_return; \
+) | dbus-launch timeout $duration ./build/xmbshell | tee build/test-log.txt
 
 frames=$(ls -1q $DREAMRENDER_HEADLESS_OUTPUT_DIR | wc -l)
 framerate=$(($frames / $duration))
